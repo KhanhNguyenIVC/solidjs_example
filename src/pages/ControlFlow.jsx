437188c6f5531feb79d6243e7dd1294ed7924f1e
ISB-VIETNAM\khanhnguyen.446 <khanhnguyen.446@vn.isb.co.jp>
2023-10-23 10:12:12 +0700

    docs: update control flow

--- src/pages/ControlFlow.jsx
+++ src/pages/ControlFlow.jsx
@@ -1,7 +1,13 @@
-import { createSignal } from "solid-js";
+import { For, createSignal } from "solid-js";
 
 const ControlFlow = () => {
     const [isLight, setIsLight] = createSignal(false)
+    const [jsFrameWorks, setJsFrameWorks] = createSignal([
+        { id: 1, name: 'Vanilla JS', img: '/images/vanilla.png' },
+        { id: 2, name: 'Solid JS', img: '/images/solid.svg' },
+        { id: 3, name: 'Vue JS', img: '/images/vue.svg' },
+        { id: 4, name: 'React JS', img: '/images/react.svg' }
+    ])
 
     return (
         <>
@@ -10,12 +16,41 @@ const ControlFlow = () => {
                 when={!isLight()}
                 fallback={<div>
                     <button onClick={() => setIsLight(false)}>Turn off the light</button>
-                    <img src="/images/bulb-on.png" alt="The light on" width={50} height={70}/>
+                    <img src="/images/bulb-on.png" alt="The light on" width={50} height={70} />
                 </div>}
             >
                 <button onClick={() => setIsLight(true)}>Turn on the light</button>
-                <img src="/images/bulb-off.png" alt="The light off" width={50} height={70}/>
+                <img src="/images/bulb-off.png" alt="The light off" width={50} height={70} />
             </Show>
+
+            <div class="for-card">
+                <span class="text-focus">&#60;For&#62;</span> component
+                <For each={jsFrameWorks()}>{(jsFrameWork, i) =>
+                    <div class="grid-view">
+                        <span style={'width: 330px'}>{i() + 1}. {jsFrameWork.name}</span><image width={25} height={20} src={jsFrameWork.img} />
+                    </div>
+                }</For>
+            </div>
+
+            <style>{`
+                .text-focus {
+                    color: #e17f7d;
+                }
+                .for-card {
+                    text-align: left;
+                    border: 1px solid #ffffff;
+                    margin-top: 1rem;
+                    padding: 10px;
+                }
+                
+                .grid-view {
+                    display: grid;
+                    grid-template-columns: 85px auto;
+                }
+                .grid-view:first-of-type {
+                    margin-top: 7px;
+                }
+            `}</style>
         </>
     )
 }
