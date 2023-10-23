5b31bb649937cd261d0fe2d13eb0a7a10cd8ce04
ISB-VIETNAM\khanhnguyen.446 <khanhnguyen.446@vn.isb.co.jp>
2023-10-23 15:09:21 +0700

    docs: update control flow

--- src/pages/ControlFlow.jsx
+++ src/pages/ControlFlow.jsx
@@ -1,13 +1,56 @@
-import { For, createSignal } from "solid-js";
+import { For, Index, Switch, Match, createSignal } from "solid-js";
+import "./ControlFlow.css";
 
 const ControlFlow = () => {
     const [isLight, setIsLight] = createSignal(false)
     const [jsFrameWorks, setJsFrameWorks] = createSignal([
-        { id: 1, name: 'Vanilla JS', img: '/images/vanilla.png' },
-        { id: 2, name: 'Solid JS', img: '/images/solid.svg' },
-        { id: 3, name: 'Vue JS', img: '/images/vue.svg' },
-        { id: 4, name: 'React JS', img: '/images/react.svg' }
+        { name: 'Vanilla JS', img: '/images/vanilla.png' },
+        { name: 'Solid JS', img: '/images/solid.svg' },
+        { name: 'Vue JS', img: '/images/vue.svg' },
+        { name: 'React JS', img: '/images/react.svg' }
     ])
+    const [bmis] = createSignal([
+        { name: 'Underweight', score: '<18.5', img: '/images/underweight.png' },
+        { name: 'Healthy', score: '18.5-24.9', img: '/images/healthy.png' },
+        { name: 'Overweight', score: '25-29.9', img: '/images/overweight.png' },
+        { name: 'Obesity', score: '>30', img: '/images/obesity.png' },
+    ])
+    const [weight, setWeighht] = createSignal('')
+    const [height, setHeight] = createSignal('')
+    const [bmi, setBmi] = createSignal(null)
+    const [bmiScore, setBmiScore] = createSignal(null)
+    const [isErrorInput, setIsErrorInput] = createSignal(false)
+
+    const getBMI = () => {
+        if (weight() === '' || height() === '') {
+            setIsErrorInput(true);
+            return;
+        }
+        setIsErrorInput(false);
+        setBmiScore(weight() / (height() * height()))
+        var bmiTemp = bmiScore()
+
+        switch (true) {
+            case (bmiTemp < 18.5):
+                // Underweight
+                setBmi(0)
+                break
+            case (bmiTemp >= 18.5 && bmiTemp <= 24.9):
+                // Normal weight
+                setBmi(1)
+                break
+            case (bmiTemp >= 25 && bmiTemp <= 29.9):
+                // Overweight
+                setBmi(2)
+                break
+            case (bmiTemp >= 30):
+                //Obesity
+                setBmi(3)
+                break
+            default:
+                setBmi(null)
+        }
+    }
 
     return (
         <>
@@ -32,25 +75,39 @@ const ControlFlow = () => {
                 }</For>
             </div>
 
-            <style>{`
-                .text-focus {
-                    color: #e17f7d;
-                }
-                .for-card {
-                    text-align: left;
-                    border: 1px solid #ffffff;
-                    margin-top: 1rem;
-                    padding: 10px;
-                }
-                
-                .grid-view {
-                    display: grid;
-                    grid-template-columns: 85px auto;
-                }
-                .grid-view:first-of-type {
-                    margin-top: 7px;
-                }
-            `}</style>
+            <div class="for-card">
+                <span class="text-focus">&#60;Switch&#62;</span> component <br />
+                Get Body Mass Index (BMI)
+                <div style={'margin-top:7px'}>
+                    <label style={'margin-top: 5px'} for="username">Weight (kg)</label><br />
+                    <input type="text" onChange={(e) => setWeighht(e.target.value)} /><br />
+                    <label for="username">Height (m)</label><br />
+                    <input type="text" onChange={(e) => setHeight(e.target.value)} /><br />
+                    <div style={isErrorInput() ? '' : 'display:none'}><span class="error-text">👉Please enter the weight & the height.</span></div>
+                    <button style={'margin-top:7px'} onClick={() => getBMI()}>Excute</button>
+                </div>
+                <div>
+                    <div className="header-body-mass">
+                        BODY MASS INDEX
+                    </div>
+                    <div class="grid-container">
+                        <Index each={bmis()}>{(bmiEle, i) =>
+                            <div class={bmi() === i ? 'active-body-mass' : ''}>
+                                <img src={bmiEle().img} width={92} height={200} />
+                                <div class="card-body-mass">{bmiEle().score}</div>
+                                <div class="text-body-mass">{bmiEle().name}</div>
+                            </div>
+                        }</Index>
+                    </div>
+                    <br />
+                </div>
+                <Switch>
+                    <Match when={bmi() === 0}>👉Your BMI is {bmiScore().toFixed(2)} You're underweight.</Match>
+                    <Match when={bmi() === 1}>👉Your BMI is {bmiScore().toFixed(2)} You're healthy.</Match>
+                    <Match when={bmi() === 2}>👉Your BMI is {bmiScore().toFixed(2)} You're overweight.</Match>
+                    <Match when={bmi() === 3}>👉Your BMI is {bmiScore().toFixed(2)} You're obesity</Match>
+                </Switch>
+            </div>
         </>
     )
 }
